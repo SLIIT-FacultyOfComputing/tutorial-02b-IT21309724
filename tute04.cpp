cfd77e3cf8e45db35d16dacebff75a81217a200e
Eranda <95522303+IT21309724@users.noreply.github.com>
2022-02-23 11:01:50 +0000

    completed

--- tute04.cpp
+++ tute04.cpp
@@ -16,14 +16,25 @@ Do not modify the main function.*/
 long Factorial(int no);
 long nCr(int n, int r);
 
-int main() {
+int main()
+{
   int n, r;
+  
   std::cout << "Enter a value for n ";
   std::cin >> n;
+  
   std::cout << "Enter a value for r ";
   std::cin >> r;
+  
   std::cout << "nCr = ";
+  
   std::cout << nCr(n,r);
+  
   std::cout << std::endl;
+
   return 0;
+}
+long nCr(int n, int r)
+{
+  
 }
